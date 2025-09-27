50ea16c11538116ad18491f358d0a99bf891485c
Vadym_Danyliuk <jekone13@outlook.com>
2025-09-27 10:59:17 +0300

    jk

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,25 @@
-import { NavLink } from 'react-router-dom';
-import styles from './Header.module.css';
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <NavLink to="/" className={styles.logo}>
-          RentalCar
+          Rental
         </NavLink>
         <nav className={styles.nav}>
-          <NavLink 
-            to="/" 
-            className={({ isActive }) => 
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
               isActive ? `${styles.link} ${styles.active}` : styles.link
             }
           >
             Home
           </NavLink>
-          <NavLink 
-            to="/catalog" 
-            className={({ isActive }) => 
+          <NavLink
+            to="/catalog"
+            className={({ isActive }) =>
               isActive ? `${styles.link} ${styles.active}` : styles.link
             }
           >
